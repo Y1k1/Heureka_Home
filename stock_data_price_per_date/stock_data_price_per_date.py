14e8eb188640742c2556f9c54a5ffd50ec978553
Y1k1 <110573233+Y1k1@users.noreply.github.com>
2024-03-24 19:02:51 +0900

    Update stock_data_price_per_date.py

--- stock_data_price_per_date/stock_data_price_per_date.py
+++ stock_data_price_per_date/stock_data_price_per_date.py
@@ -1,3 +1,56 @@
+import os
+import io
+import zipfile
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+
+script_path = os.path.abspath(__file__)
+script_dir = os.path.dirname(script_path)
+os.chdir(script_dir)
+
+# Load client secrets from the credentials.json file
+credentials = service_account.Credentials.from_service_account_file(
+    'credentials.json',
+    scopes=['https://www.googleapis.com/auth/drive']
+)
+
+# Build the Google Drive API client
+drive_service = build('drive', 'v3', credentials=credentials)
+
+def download_file_from_drive(file_name, folder_id):
+    # Search for the file in the specified folder
+    response = drive_service.files().list(
+        q=f"name='{file_name}' and '{folder_id}' in parents",
+        spaces='drive',
+        fields='files(id, name)'
+    ).execute()
+    files = response.get('files', [])
+
+    if files:
+        file_id = files[0].get('id')
+        request = drive_service.files().get_media(fileId=file_id)
+        fh = io.BytesIO()
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print(f"Download {int(status.progress() * 100)}%.")
+        fh.seek(0)
+
+        with open(file_name, 'wb') as f:
+            f.write(fh.read())
+        print(f'Downloaded {file_name}')
+    else:
+        print(f'File {file_name} not found in folder {folder_id}')
+
+# Specify the name of the file and the Google Drive folder ID
+file_name = 'stock_data_var2_complete.csv'
+folder_id = '1AMu-_CnZE07uwk57Hb-ZzL9wthrQUQX1'
+
+# Download the file
+download_file_from_drive(file_name, folder_id)
+
 import csv
 import os
 import requests
